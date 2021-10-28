2d98eac8f0e90cd18825bae206a47c43137f421e
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 17:17:35 +0530

    Update forwarder.py

--- BD/plugins/forwarder.py
+++ BD/plugins/forwarder.py
@@ -3,7 +3,7 @@ from telethon.sync import events
 
 @tbot.on(events.NewMessage(incoming=True))
 async def x(e):
- if e.chat.id ==1686303934:
+ if e.chat.id ==1381212488 and e.sender.id ==1354488699:
   try:
     for x in [-1001176306346, -1001443691244]:
         await e.forward_to(x)
