afc497cf5e701db6962856f59258be4b64568296
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 15:08:06 +0530

    Update __main__.py

--- BD/__main__.py
+++ BD/__main__.py
@@ -1,12 +1,39 @@
-from BD import tbot,TOKEN
-import sys
+# (c) Copyright 21-22 lucifeermorningstar@GitHub, < https://GitHub.com/lucifeermorningstar >
+# written By Devil
 
-try:
-  tbot.start(bot_token=TOKEN)
-  
-except:
-  print('invalid token')
-  sys.exit()
-  
-  
-tbot.run_until_disconnected()
+
+import sys
+import glob
+import logging
+import importlib
+from pathlib import Path
+from . import tbot
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+
+def load_plugins(plugin_name):
+    path = Path(f"BD/plugins/{plugin_name}.py")
+    name = "BD.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["BD.plugins." + plugin_name] = load
+    print("IMPORTED --> " + plugin_name)
+
+
+path = "BD/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        thepath = Path(a.name)
+        plugin_name = thepath.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("Bot Started Successfully ")
+
+
+if __name__ == "__main__":
+    tbot.run_until_disconnected()
