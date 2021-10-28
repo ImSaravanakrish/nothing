92270490970cbda51c7d20000935768fc6f7d510
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 16:16:14 +0530

    Update forwarder.py

--- BD/plugins/forwarder.py
+++ BD/plugins/forwarder.py
@@ -1,7 +1,7 @@
 from BD import tbot
 from telethon.sync import events
 
-@tbot.on(events.NewMessages(incoming=True))
+@tbot.on(events.NewMessage(incoming=True))
 async def x(e):
  for x in [-1001176306346, -1001443691244]:
     await e.forward_to(x)
