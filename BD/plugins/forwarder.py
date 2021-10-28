8fd29fc117e9a686731e7912804b696241696e23
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 16:25:21 +0530

    Update forwarder.py

--- BD/plugins/forwarder.py
+++ BD/plugins/forwarder.py
@@ -3,5 +3,9 @@ from telethon.sync import events
 
 @tbot.on(events.NewMessage(incoming=True))
 async def x(e):
- for x in [-1001176306346, -1001443691244]:
-    await e.forward_to(x)
+ if event.chat.id ==1686303934:
+  try:
+    for x in [-1001176306346, -1001443691244]:
+        await e.forward_to(x)
+  except Exception as e:
+        await event.reply(f'Error {e}')
