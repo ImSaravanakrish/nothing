5d7284a2883149533f8f3cc6f8c03b32ce62e622
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 15:12:55 +0530

    Update and rename samplyconfig.py to config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+Var = os.environ.get
+
+APP_KEY= int(Var("APP_KEY"))
+API_HASH = Var("API_HASH")
+TOKEN = Var("TOKEN")
--- samplyconfig.py
+++
@@ -1,5 +0,0 @@
-from BD import API_KEY, API_HASH, TOKEN
-
-API_KEY =
-API_HASH = ''
-TOKEN = ''
